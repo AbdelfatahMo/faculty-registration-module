f3beb7d1bc9aadc4d6a8c0da5855efc1ca8c1dfd
AbdelfatahMo <b5b71999@outlook.com>
2023-07-15 20:30:14 +0300

    Improve Application view and model

--- __manifest__.py
+++ __manifest__.py
@@ -6,22 +6,22 @@
 
     'description': "Control the process of registrations and save student information",
 
-    'author': "Obadoo",
-    'website': "abdelfatah_mohamed@w-ist.com",
-
+    'author': "Abdelfatah Mohamad",
+    'website': "abdelfatah.mohamad.99@gmail.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Registration/Faculty',
     'version': '15.0',
     'license': 'AGPL-3',
-    'sequence': '-10',
+    'sequence': '-3',
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','mail'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'data/sequence.xml',
         'views/application.xml',
         'views/family.xml',
         'views/faculty.xml',
--- models/application.py
+++ models/application.py
@@ -1,12 +1,22 @@
-from odoo import fields, models
+from datetime import datetime
+from odoo import fields, models, api, _
 
 
 class ApplicationInfo(models.Model):
     _name = 'application.info'
     _description = 'Application Information'
+    _inherit = ["mail.thread", "mail.activity.mixin"]
 
     _rec_name = 'name'
     _order = 'name ASC'
+
+    _sql_constraints = [
+        (
+            'national_id_unique',
+            'unique(national_id)',
+            _('Please make sure about your National ID')
+        )
+    ]
 
     name = fields.Char(
         required=True,
@@ -15,85 +25,97 @@ class ApplicationInfo(models.Model):
 
     address = fields.Char(string="City", required=True)
 
-    national_number = fields.Integer(
-        required=True
+    national_id = fields.Integer(
+        # required=True
     )
 
     pesonal_image = fields.Binary(
-        required=True
+        # required=True
     )
 
     age = fields.Integer(
-        required=True
+        compute="_compute_age",
     )
 
     date_of_birth = fields.Date(
-        required=True
+        
+        default=datetime(2005,1,1)       
+        # required=True
+    )
+
+    ref = fields.Char(
+        readonly=True,
     )
 
     height = fields.Float(
-        required=True
+        # required=True
     )
 
     weight = fields.Float(
-        required=True)
+        # required=True
+    )
 
     secondary_certification_image = fields.Binary(
-        required=True
+        # required=True
     )
 
     degree_with_precent = fields.Float(
-        required=True,
+        # required=True,
         default=0
     )
 
     degree_with_numbers = fields.Float(
-        required=True,
+        # required=True,
         default=0
     )
 
     number_of_family = fields.Integer(
-        required=True
+        # required=True
     )
 
     memeber_of_family_ids = fields.Many2many(
-        "family.memeber",
+        comodel_name="family.memeber",
         string="Name"
-        )
+    )
 
-    family_memeber_age = fields.Integer(string="Age")
-
-    faculty_ids = fields.Many2many(
-        'registration.faculty',
+    desired_faculty_ids = fields.Many2many(
+        comodel_name='registration.faculty',
         string='Desired Faculties',
-        required=True
+        # required=True
     )
 
     expected_faculty = fields.Reference(
-        [('registration.faculty', 'Faculty')],
-    )
-
-    desired_faculty = fields.Reference(
-        [("registration.faculty", 'Faculty')],
-        required=True
+        selection=[('registration.faculty', 'Faculty')],
     )
 
     reason_of_desired = fields.Text(
-        required=True
+        # required=True
     )
 
     skill_ids = fields.Many2many(
-        "student.skill",
+        comodel_name="student.skill",
         string='Skills',
-        required=True
+        # required=True
     )
 
+    @api.model
+    def create(self, values):
+        result = super(ApplicationInfo, self)
+        values['ref'] = self.env['ir.sequence'].next_by_code(
+            "application.info")
+        return result.create(values)
 
-    
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        today = datetime.today()
+        for record in self:
+            record.age = today.year - record.date_of_birth.year - \
+                ((record.date_of_birth.month, record.date_of_birth.day)
+                 > (today.month, today.day))
+        
+
 
 # @api.depends('depends')
 # def _compute_age(self):
-#     for record in self:   
+#     for record in self:
 #         record.age = something
-
-
